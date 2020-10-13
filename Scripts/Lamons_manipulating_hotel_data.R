c3afae63a0227dbe094af05246a64736fea560ad
EthanLamons <elamons2@illinois.edu>
2020-10-13 15:12:22 -0500

    Adding a section which saves a file with the means

--- Scripts/Lamons_manipulating_hotel_data.R
+++ Scripts/Lamons_manipulating_hotel_data.R
@@ -15,7 +15,7 @@ adr_by_party <- hotels %>%
   mutate(party_size = (adults + children + babies)) %>%
   mutate(Number_of_Kids_per_Party = (children + babies)) %>%
   arrange(party_size) %>%
-  filter(adr < 500 & adr != 0 & party_size < 10) %>%
+  filter(adr < 500 & adr != 0 & party_size < 10 & party_size > 0) %>%
   na.omit(adr_by_party)
   
 # I also removed adr = 0 because those are useless to my analysis
@@ -53,12 +53,12 @@ hotels %>%
         axis.title.y = element_text(size = 15, vjust = 2),
         title = element_text(size = 14),
         legend.title = element_text(size = 12)) +
-  geom_abline(aes(slope = 31.60, intercept = 41.13), size = 1.5) +
+  geom_abline(aes(slope = 31.64, intercept = 41.06), size = 1.5) +
   scale_x_continuous(n.break = 6) +
   annotate(geom = "segment", x = 2, y = 150, xend = 2.5, yend = 130, size = 1, colour = "black", arrow = arrow(length = unit(2, "mm"))) +
   annotate(geom = "text", x = 0.52, y = 182, label = "Linear Regression Line", hjust = "left", colour = "black") +
-  annotate(geom = "text", x = 0.52, y = 167, label = "(yint = 41.13, slope = 31.60, p < 2.2e-16,", hjust = "left", colour = "black") +
-  annotate(geom = "text", x = 0.52, y = 152, label = "paste(italic(R) ^ 2, \"= 0.1911)\")", parse = TRUE, hjust = "left", colour = "black"))
+  annotate(geom = "text", x = 0.52, y = 167, label = "(yint = 41.06, slope = 31.64, p < 2.2e-16,", hjust = "left", colour = "black") +
+  annotate(geom = "text", x = 0.52, y = 152, label = "paste(italic(R) ^ 2, \"= 0.191)\")", parse = TRUE, hjust = "left", colour = "black"))
 
 
 
@@ -68,15 +68,15 @@ hotels %>%
   scale_color_distiller(palette = "Paired") +
   labs(x = "Number of Kids per Booking", y = "Average Daily Rate", title = "Number of Kids per Booking vs ADR", caption = "Graph made by - Ethan Lamons") +
   dark_theme_bw() +
-  geom_abline(slope = 39.99, intercept = 98.99, size = 1.5) +
+  geom_abline(slope = 40.01, intercept = 99, size = 1.5) +
   scale_x_continuous(n.break = 6) +
   theme(axis.title.x = element_text(size = 15),
         axis.title.y = element_text(size = 15, vjust = 2),
         title = element_text(size = 20)) +
   annotate(geom = "segment", x = 0.5, y = 200, xend = 1, yend = 150, size = 1, colour = "black", arrow = arrow(length = unit(2, "mm"))) +
   annotate(geom = "text", x = -.4, y = 230, label = "Linear Regression Line", hjust = "left", colour = "black") +
-  annotate(geom = "text", x = -.4, y = 215, label = "(yint = 98.99, slope = 39.99, p < 2.2e-16,", hjust = "left", colour = "black") +
-  annotate(geom = "text", x = 0.9, y = 215, label = "paste(italic(R) ^ 2, \" = 0.1293)\")", parse = TRUE, hjust = "left", colour = "black"))
+  annotate(geom = "text", x = -.4, y = 215, label = "(yint = 99, slope = 40.01, p < 2.2e-16,", hjust = "left", colour = "black") +
+  annotate(geom = "text", x = 0.9, y = 215, label = "paste(italic(R) ^ 2, \" = 0.124)\")", parse = TRUE, hjust = "left", colour = "black"))
 
 
 # Finding the linear model for both graphs
@@ -87,4 +87,6 @@ lm.nkids <- lm(adr_by_party$adr ~ adr_by_party$Number_of_Kids_per_Party)
 # in both cases indicating high significance between the variables
 summary(lm.party_size)
 summary(lm.nkids)
+
+#Writing the means into a txt file
 
